11722d202f5f217372df3538db3462a92bb16d0b
Rafael Abreu <rafael.abreu031015@gmail.com>
2024-10-21 23:14:22 -0300

    finish project

--- comands.py
+++ comands.py
@@ -7,7 +7,8 @@ def checar_banco():
                     nome VARCHAR(255),
                     tel VARCHAR(15) NOT NULL);''')
         
-        cursor.execute('''INSERT INTO contatos (id, nome, tel) VALUES ('1', 'Rafael Abreu', '71999048364');''')
+        cursor.execute('''INSERT INTO contatos (id, nome, tel)
+                       VALUES ('1', 'Rafael Abreu', '71999048364');''')
 
     except:
         print('Algo não deu certo!')
@@ -16,7 +17,7 @@ def novo_contato():
     try:
         tel = int(input('Digite o telefone: '))
 
-        cursor.execute('''SELECT c.tel FROM contatos c WHERE tel = %s''', (tel,))
+        cursor.execute('SELECT c.tel FROM contatos c WHERE tel = %s', (tel,))
         exists = cursor.fetchone()
 
         if exists:
@@ -24,26 +25,26 @@ def novo_contato():
 
         else:
             nome = input('Digite o nome: ')
-            cursor.execute('''INSERT INTO contatos(nome, tel) VALUES (%s, %s)''', (nome, tel))
+            cursor.execute('INSERT INTO contatos(nome, tel) VALUES (%s, %s)', (nome, tel))
             print('Contato salvo com sucesso!')
 
     except ValueError:
         print('Por favor, digite um valor inteiro!')
 
-def editar_contato():
+def atualizar_contato():
     
     try:
         tel = int(input('Digite o telefone que deseja editar: '))
 
-        cursor.execute('''SELECT c.tel FROM contatos c WHERE tel = %s''', (tel,))
+        cursor.execute('SELECT c.tel FROM contatos c WHERE tel = %s', (tel,))
         exists = cursor.fetchone()
 
         if exists:
             try:
                 novoTel = int(input('Digite o novo telefone: '))
                 nome = input('Digite o nome: ')
-                cursor.execute('''INSERT INTO contatos(nome, tel) VALUES (%s, %s)''', (nome, novoTel))
-                print('Contato editado com sucesso!')
+                cursor.execute('INSERT INTO contatos(nome, tel) VALUES (%s, %s)', (nome, novoTel))
+                print('Contato atualizar com sucesso!')
             except ValueError:
                 print('Por favor, digite um valor inteiro!')
         
@@ -53,12 +54,32 @@ def editar_contato():
     except ValueError:
         print('Por favor, digite um valor inteiro!')
 
+def contultar_contatos():
+    cursor.execute('SELECT * from contatos')
+    result = cursor.fetchall()
+
+    for contato in result:
+        id, nome, tel = contato
+        print(f'ID: {id} | Nome: {nome} | Tel: {tel}')
+
+
 def meu_contato():
-    cursor.execute('''SELECT c.tel FROM contatos c WHERE id=1''')
+    cursor.execute('SELECT c.tel FROM contatos c WHERE id=1')
     result = cursor.fetchone()
-    print(result)
+
+    print(f'Meu contato: {result}')
 
     cursor.close()
 
-def editar_meu_contato():
-    pass
+def atualizar_meu_contato():
+
+    try:
+        novo_contato = int(input('Digite o novo número: '))
+        cursor.execute('UPDATE contatos SET tel = %s WHERE id=1', (novo_contato,))
+        print('Contato atualizado com sucesso!')
+
+    except TypeError:
+        print('Por favor, digite um valor inteiro!')
+
+    except Exception as e:
+        print(f'Ocorreu um erro: {e}')
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import os
 from comands import (
     checar_banco,
     novo_contato,
-    editar_contato,
-    meu_contato
+    atualizar_contato,
+    contultar_contatos,
+    meu_contato,
+    atualizar_meu_contato
 )
 
 os.system('cls' if os.name=='nt' else 'clear')
@@ -12,7 +14,7 @@ os.system('cls' if os.name=='nt' else 'clear')
 checar_banco()
 
 while True:
-    opt = int(input('\nO que deseja?\n1 - Novo contato\n2 - Editar contato\n3 - Consultar contatos\n4 - Consultar meu número\n5 - Editar meu número\n0 - Sair\n'))
+    opt = int(input('\nO que deseja?\n1 - Novo contato\n2 - Atualizar contato\n3 - Consultar contatos\n4 - Consultar meu número\n5 - Atualizar meu número\n0 - Sair\n'))
 
     match opt:
         case 1:
@@ -21,18 +23,18 @@ while True:
 
         case 2:
             os.system('cls' if os.name=='nt' else 'clear')
-            editar_contato()
+            atualizar_contato()
 
         case 3:
             os.system('cls' if os.name=='nt' else 'clear')
-            pass
+            contultar_contatos()
 
         case 4:
             os.system('cls' if os.name=='nt' else 'clear')
             meu_contato()
         case 5:
             os.system('cls' if os.name=='nt' else 'clear')
-            pass
+            atualizar_meu_contato()
 
 
         case 0:
